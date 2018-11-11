35059e8eea5194f3f10879f86e954450f6511060
AhGhanima <25593897+AhGhanima@users.noreply.github.com>
2018-11-12 00:19:58 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Websocket from 'react-websocket';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        ticker: "ETHBTC",
+        price: 0
+      };
+    }
+
+  handleData(data) {
+    let result = JSON.parse(data);
+    this.setState({price: result.p});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div id="app">
+          <div className="center border">
+            <span className="text">{this.state.price}</span>
+          </div>
+          <div className="text center">
+          {this.state.ticker}
+          </div>
+          <Websocket url={`wss://stream.binance.com:9443/ws/${this.state.ticker.toLowerCase()}@aggTrade`}
+                        onMessage={this.handleData.bind(this)} debug={true}/>
+        </div>
       </div>
     );
   }
